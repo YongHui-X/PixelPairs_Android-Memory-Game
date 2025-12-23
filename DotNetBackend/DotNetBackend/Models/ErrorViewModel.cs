0032ffaae85c7bc5d813be122871ef20a94d37d9
NightlyAffair <nithvinlee@gmail.com>
2025-12-23 11:39:42 +0800

    init dotnet backend

---
+++ DotNetBackend/DotNetBackend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DotNetBackend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
