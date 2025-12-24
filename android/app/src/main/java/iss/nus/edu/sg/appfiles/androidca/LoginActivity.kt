886c2b014b4cb1664b4eb9f97db168d074098cca
Glenn Min <minglenn@outlook.com>
2025-12-24 23:41:20 +0800

    update activity_login.xml update strings.xml new LoginActivity.kt new colors.xml delete themes.xml

--- android/app/src/main/java/iss/nus/edu/sg/appfiles/androidca/LoginActivity.kt
+++ android/app/src/main/java/iss/nus/edu/sg/appfiles/androidca/LoginActivity.kt
@@ -1,20 +1,61 @@
 package iss.nus.edu.sg.appfiles.androidca
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import iss.nus.edu.sg.appfiles.androidca.models.Users
+import android.content.Intent
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var login: Button
+
+    private val users = listOf(
+        Users("Tin","test",true),
+        Users("CherWah","test",false),
+        Users("Michael","test",false)
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        username = findViewById<EditText>(R.id.login_username)
+        password = findViewById<EditText>(R.id.login_password)
+        login = findViewById<Button>(R.id.btn_login)
+
+        login.setOnClickListener {
+            val username = username.text.toString()
+            val password = password.text.toString()
+
+            if (username.isEmpty() || password.isEmpty()){
+                Toast.makeText(this, "Please enter username and password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            auth(username, password)
+        }
+    }
+
+    private fun auth(username: String, password: String){
+        val user = users.find {
+            it.username == username && it.password == password
+        }
+        if (user != null) {
+            Toast.makeText(this, "Login successful.", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, FetchActivity::class.java)
+            intent.putExtra("username", user.username)
+            intent.putExtra("isPaid", user.isPaid)
+            startActivity(intent)
+            finish()
+        } else {
+            Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show()
         }
     }
 }
