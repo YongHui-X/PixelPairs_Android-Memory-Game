fbd54e900c72ec0295e5444cc78e51f4d401beb1
TonySongJQ <songjiaqi0704@gmail.com>
2025-12-27 16:41:37 +0800

    Add Play screen UI layout
    Added:
    
    1. Layout:
    activity_play.xml
    item_card.xml
    
    2. drawable:
    image and icons
    
    3. models:
    some test demo in PlayActivity (can be delete)

--- android/app/src/main/java/iss/nus/edu/sg/appfiles/androidca/PlayActivity.kt
+++ android/app/src/main/java/iss/nus/edu/sg/appfiles/androidca/PlayActivity.kt
@@ -1,20 +1,24 @@
 package iss.nus.edu.sg.appfiles.androidca
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class PlayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_play)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val tvMatches = findViewById<TextView>(R.id.Matches)
+        val tvTimer = findViewById<TextView>(R.id.Timer)
+        val rvCards = findViewById<RecyclerView>(R.id.rvCards)
+
+        rvCards.layoutManager = GridLayoutManager(this, 3)
+
     }
 }
