0ae4e69dd296b8eb8d5839bae5301890fc0aa18d
TonySongJQ <songjiaqi0704@gmail.com>
2026-01-05 10:31:22 +0800

    db

--- DotNetBackend/DotNetBackend/Data/Constants.cs
+++ DotNetBackend/DotNetBackend/Data/Constants.cs
@@ -2,6 +2,6 @@
 {
     public class Constants
     {
-        public static string CONNECTION_STRING = @"server=localhost;uid=springstudent;pwd=springstudent;database=androidca";
+        public static string CONNECTION_STRING = @"server=localhost;uid=root;pwd=Songjiaqi071012;database=androidca";
     }
 }
