37e165470e4338e466dadb559a8a8bdfbf0c1708
TonySongJQ <songjiaqi0704@gmail.com>
2026-01-05 13:14:57 +0800

    co

--- DotNetBackend/DotNetBackend/Data/Constants.cs
+++ DotNetBackend/DotNetBackend/Data/Constants.cs
@@ -2,10 +2,6 @@
 {
     public class Constants
     {
-<<<<<<< HEAD
         public static string CONNECTION_STRING = @"server=localhost;uid=root;pwd=Songjiaqi071012;database=androidca";
-=======
-        public static string CONNECTION_STRING = @"server=localhost;uid=root;pwd=246810;database=androidca";
->>>>>>> refs/remotes/origin/Jason_v2
     }
 }
