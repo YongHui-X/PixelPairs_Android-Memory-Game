fe19f7a4ef9fbf8357fc363d7600c28d3a04ed4b
zhouq <zhouqian2123@gmail.com>
2026-01-05 13:58:23 +0800

    Newest version

--- DotNetBackend/DotNetBackend/Data/Constants.cs
+++ DotNetBackend/DotNetBackend/Data/Constants.cs
@@ -2,6 +2,6 @@
 {
     public class Constants
     {
-        public static string CONNECTION_STRING = @"server=localhost;uid=root;pwd=Zayar2002;database=AndroidCA";
+        public static string CONNECTION_STRING = @"server=localhost;uid=root;pwd=246810;database=AndroidCA";
     }
 }
